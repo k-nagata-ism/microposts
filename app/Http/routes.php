908cb5ce3529d484c6684d31c343317235b9c750
k_nagata <k_nagata@ism.co.jp>
2018-04-24 09:56:18 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('/signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
 
 Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
