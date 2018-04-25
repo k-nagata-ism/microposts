136ccb5e9950fa72b4ac87dd8997952b92ffe5b9
k_nagata <k_nagata@ism.co.jp>
2018-04-25 03:59:18 +0000

    post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,11 +9,15 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+*/
+
+Route::get('/', 'WelcomeController@index');
 Route::get('/signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('/signup', 'Auth\AuthController@postRegister')->name('signup.post');
 
@@ -24,4 +28,5 @@ Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
